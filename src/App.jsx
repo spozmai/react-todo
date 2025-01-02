dabc2405d6b4b7decdd3c24468f630a74ea5040e
Spozmai <spozmai.helali@gmail.com>
2025-01-02 18:31:44 -0500

    lesson1-7

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: [
+              { id: 1, title: "Learn React" },
+              { id: 2, title: "Build a Todo App" },
+            ],
+          },
+        });
+      }, 2000);
+    });
+
+    fetchData.then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
 
   const addTodo = (newTodo) => {
     setTodoList((prevTodos) => [...prevTodos, newTodo]);
@@ -16,7 +43,7 @@ function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
       <AddTodoForm onAddTodo={addTodo} />
     </>
   );
