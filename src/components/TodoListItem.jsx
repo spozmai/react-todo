efc37195e8bdaecb27eb33b99cea4aae04c4cf58
Spozmai <spozmai.helali@gmail.com>
2025-02-25 22:08:53 -0500

    lesson-15

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,17 +1,38 @@
-import React from "react";
-import PropTypes from 'prop-types';  
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import styles from "./TodoListItem.module.css";
 
-const TodoListItem = ({ todo }) => {
-  return <li className={styles.ListItem}>{todo.title}</li>;
-};
+const TodoListItem = ({ todo, onRemoveTodo }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
 
+  const handleDelete = async () => {
+    if (isDeleting) return; // Prevent duplicate clicks
+
+    setIsDeleting(true);
+    await onRemoveTodo(todo.id);
+    setIsDeleting(false);
+  };
+
+  return (
+    <li className={styles.ListItem}>
+      {todo.title}
+      <button 
+        onClick={handleDelete} 
+        disabled={isDeleting}
+        className={styles.DeleteButton}
+      >
+        {isDeleting ? "Removing..." : "Remove"}
+      </button>
+    </li>
+  );
+};
 
 TodoListItem.propTypes = {
   todo: PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,  
-    title: PropTypes.string.isRequired,  
-  }).isRequired,  
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  onRemoveTodo: PropTypes.func.isRequired,
 };
 
 export default TodoListItem;
